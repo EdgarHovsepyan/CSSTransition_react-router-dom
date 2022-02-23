fd33d63a8b3536a261995a7a63059f89d86001f4
Edgar <hovsepyan.edgar@betconstruct.com>
2022-02-23 18:31:02 +0400

    init

---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Title from '../components/Title'
+
+const About = () => {
+  return (
+     <div className='inner about'>
+      <Title lineContent={"This is the"} lineContent2={"About page"} />
+      <div>
+        <p className='info'>  
+          Bed. She merely cache. He how those the was tricoloured initial in
+           frequency systems he rank such monstrous succeeded serving his neighbours 
+           lay that effort the great. In is said even building analyzed revisit they
+            this big for into real the is right, will client a not by phase.</p>
+      </div>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Title from '../components/Title'
+
+const Home = () => {
+  return (
+    <div className='inner home'>
+      <Title lineContent={"This is the"} lineContent2={"Home page"} />
+      <div>
+        <p className='info'>  
+          Bed. She merely cache. He how those the was tricoloured initial in
+           frequency systems he rank such monstrous succeeded serving his neighbours 
+           lay that effort the great. In is said even building analyzed revisit they
+            this big for into real the is right, will client a not by phase.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Home
